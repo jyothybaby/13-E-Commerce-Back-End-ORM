4c6eb71d60e3322142d07aa039c971a45df9c376
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-09-21 13:29:33 -0400

    Updated the fully functtional code for product Api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,22 +61,21 @@ router.put('/:id',async (req, res) => {
 router.delete('/:id',async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (!productData) {
-      res.status(404).json({ message: 'No product found with that id!' });
+    if (!tagData) {
+      res.status(404).json({ message: 'No TAG found with that id!' });
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 module.exports = router;
-tagData
